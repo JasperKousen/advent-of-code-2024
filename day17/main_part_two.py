a22dfe3da783b30d40d79884c95d4733c1856571
JasperKousen <jasperkousen@gmail.com>
2024-12-17 22:24:34 +0100

    Day 17

--- day17/main_part_two.py
+++ day17/main_part_two.py
@@ -1,4 +1,5 @@
 import itertools
+import matplotlib.pyplot as plt
 
 
 def run(program, a=0, b=0, c=0, verbose=False):
@@ -70,26 +71,70 @@ def main():
         0, 3,  # A = A // 8
         1, 6,  # B = B XOR 6
         5, 5,  # print B % 8
-        3, 0   # GOTO 00
+        3, 0  # GOTO 00
     ]
+    # 35_184_372_088_832, 281_474_976_710_655
+    print(len(program))
 
-    # print(list(run(program=program, a=279_474_976_710_655, verbose=False)))
-    # for a in range(8**12 + 4, 8**12 + 5 * 8**12 + 1, 8**12):
-    for a in range(105689481478144, 105759274696704 + 1, 1):
-        # 105544526331904 , 114349209288704
-        # 127534758887424 , 131941395333120
-        # 136330851909632 , 140737488355328
+    def make_samples(minimum, maximum, n):
+        samples = {}
+        for a in range(
+                minimum,
+                maximum + int((maximum - minimum) / n) + 1,
+                max(1, int((maximum - minimum) / n))
+        ):
+            samples[a] = list(run(program=program, a=a, verbose=False))
+        return samples
 
-        # print(a, list(run(program=program, a=a, verbose=False)))
-        # print(a)
-        output = run(program=program, a=a)
+    def find_ranges(samples, digit):
+        ranges = []
 
-        for o, p in itertools.zip_longest(output, program):
-            if o is None or p is None or o != p:
-                break
+        in_range = False
+        prev_key = list(samples.keys())[0]
+        start = prev_key
+        for k, v in samples.items():
+            if v[digit] == program[digit]:
+                if not in_range:
+                    start = prev_key
+                    in_range = True
+            else:
+                if in_range:
+                    ranges.append((start, k))
+                    in_range = False
+            prev_key = k
         else:
-            print(program, a)
-            return
+            if in_range:
+                ranges.append((start, k))
+
+        return ranges
+
+    minimum, maximum = 35_184_372_088_832, 281_474_976_710_655
+    samples = make_samples(minimum, maximum, n=100000)
+    ranges = find_ranges(samples, digit=15)
+
+    for digit in range(15, 5, -1):
+        new_ranges = []
+        for minimum, maximum in ranges:
+            samples = make_samples(minimum, maximum, n=10000)
+            new_ranges.extend(find_ranges(samples, digit=digit))
+        else:
+            ranges = new_ranges
+
+    plt.plot(samples.keys(), [v[digit] for v in samples.values()])
+    plt.show()
+
+    print(ranges)
+
+    for minimum, maximum in ranges:
+        for a in range(minimum, maximum + 1):
+            output = run(program=program, a=a)
+
+            for o, p in itertools.zip_longest(output, program):
+                if o is None or p is None or o != p:
+                    break
+            else:
+                print(program, a)
+                return
 
 
 if __name__ == '__main__':
