ff03e1ea033a4ed30a3e094c3b6abb0da9a98f9c
jasperkousen <jasperkousen@gmail.com>
2025-12-02 10:18:55 +0100

    2025 day 02

--- 2025/day02/main_2.py
+++ 2025/day02/main_2.py
@@ -1,34 +1,17 @@
-import math
-
-
-def get_primes(n):
-
-    primes = []
-    for i in range(2, n):
-        while True:
-            for x in range(2, int(math.sqrt(i) + 1)):
-                if i % x == 0:
-                    break
-            else:
-                primes.append(i)
-
-def get_factors(n):
-    return [i for i in range(1, n // 2 + 1) if n % i == 0]
-
 def main():
     with open('input.txt') as f:
         data = f.read()
 
     ranges = [(int(d.split('-')[0]), int(d.split('-')[1])) for d in data.split(',')]
 
-    # divisors = get_primes(max(end for _, end in ranges) // 2)
-    # print(f"divisors: {len(divisors)}")
-
     def is_invalid(id):
         id = str(id)
         length = len(id)
 
-        for d in get_factors(length):
+        for d in range(1, length // 2 + 1):
+            if length % d != 0:
+                continue
+
             if d >= length:
                 return False
 
